0b825be33b7c32f248f383ab2639d777fadce9a7
Deep Lekhak <lekhakdeepak10@gmail.com>
2023-06-25 01:27:41 -0400

    final changes

---
+++ components/Meta.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+function Meta() {
+  return (
+    <Head>
+      <title>Crypto Currency Price Tracker</title>
+
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+  );
+}
+
+export default Meta;
---
+++ configs/index.js
@@ -0,0 +1,133 @@
+const CRYPTOCURRENCIES = [
+  {
+    id: "BTC",
+    name: "Bitcoin",
+    symbol: "BTCBUSD",
+    iconCode: 1,
+    price: 0,
+    prevPrice: 0,
+    highPrice: 0,
+    lowPrice: 0,
+  },
+  {
+    id: "ETH",
+    name: "Etherium",
+    symbol: "ETHBUSD",
+    iconCode: 1027,
+    price: 0,
+    prevPrice: 0,
+    highPrice: 0,
+    lowPrice: 0,
+  },
+  {
+    id: "BNB",
+    name: "BNB",
+    symbol: "BNBBUSD",
+    iconCode: 1839,
+    price: 0,
+    prevPrice: 0,
+    highPrice: 0,
+    lowPrice: 0,
+
+  },
+  {
+    id: "XRP",
+    name: "XRP",
+    symbol: "XRPBUSD",
+    iconCode: 52,
+    price: 0,
+    prevPrice: 0,
+    highPrice: 0,
+    lowPrice: 0,
+
+  },
+  {
+    id: "DOGE",
+    name: "Dogecoin",
+    symbol: "DOGEBUSD",
+    iconCode: 74,
+    price: 0,
+    prevPrice: 0,
+    highPrice: 0,
+    lowPrice: 0,
+
+  },
+  {
+    id: "MATIC",
+    name: "Polygon",
+    symbol: "MATICBUSD",
+    iconCode: 3890,
+    price: 0,
+    prevPrice: 0,
+    highPrice: 0,
+    lowPrice: 0,
+
+  },
+  {
+    id: "SOL",
+    name: "Solana",
+    symbol: "SOLBUSD",
+    iconCode: 5426,
+    price: 0,
+    prevPrice: 0,
+    highPrice: 0,
+    lowPrice: 0,
+
+  },
+  {
+    id: "SHIB",
+    name: "Shiba Inu",
+    symbol: "SHIBBUSD",
+    iconCode: 5994,
+    price: 0,
+    prevPrice: 0,
+    highPrice: 0,
+    lowPrice: 0,
+
+  },
+  {
+    id: "APE",
+    name: "ApeCoin",
+    symbol: "APEBUSD",
+    iconCode: 18876,
+    price: 0,
+    prevPrice: 0,
+    highPrice: 0,
+    lowPrice: 0,
+
+  },
+  {
+    id: "NEAR",
+    name: "NEAR Protocol",
+    symbol: "NEARBUSD",
+    iconCode: 6535,
+    price: 0,
+    prevPrice: 0,
+    highPrice: 0,
+    lowPrice: 0,
+  
+  },
+  {
+    id: "LUNC",
+    name: "Terra Classic",
+    symbol: "LUNCBUSD",
+    iconCode: 4172,
+    price: 0,
+    prevPrice: 0,
+    highPrice: 0,
+    lowPrice: 0,
+
+  },
+  {
+    id: "LUNA",
+    name: "Terra",
+    symbol: "LUNABUSD",
+    iconCode: 20314,
+    price: 0,
+    prevPrice: 0,
+    highPrice: 0,
+    lowPrice: 0,
+
+  },
+];
+export { CRYPTOCURRENCIES };
